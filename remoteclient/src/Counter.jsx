d2bee551791b1693db105a6e184739afe75fb693
Pi-Cal <nnthach1024@gmail.com>
2022-05-11 08:58:10 +0700

    view card

--- remoteclient/src/Counter.jsx
+++ remoteclient/src/Counter.jsx
@@ -1,11 +1,15 @@
 import { createSignal } from "solid-js";
 export default () => {
-  const [count, setCount] = createSignal(0);
   return (
     <div class="bg-red-300 text-white p-5">
-      <div>Count = {count()}</div>
       <div class="max-w-sm rounded overflow-hidden shadow-lg">
-        <img class="w-full" src="https://picsum.photos/seed/picsum/200/200" alt="Sunset in the mountains"></img>
+        <img class="w-full" src="https://picsum.photos/seed/picsum/400/200" alt="Sunset in the mountains"></img>
+        <div class="px-6 py-4">
+          <div class="font-bold text-xl mb-2">The Mountain</div>
+          <p class="text-gray-700 text-base">
+            Lorem ipsum dolor sit amet, consectetur adipisicing elit. Voluptatibus quia, nulla! Maiores et perferendis eaque, exercitationem praesentium nihil.
+          </p>
+        </div>
         <div class="px-6 pt-4 pb-2">
           <span class="inline-block bg-gray-200 rounded-full px-3 py-1 text-sm font-semibold text-gray-700 mr-2 mb-2">
             #photography
@@ -18,10 +22,9 @@ export default () => {
           </span>
         </div>
         <button
-          onClick={() => setCount(count() + 1)}
-          class="bg-blue-500 hover:bg-blue-600 active:bg-blue-700 focus:outline-none focus:ring focus:ring-blue-300 rounded-full p-1 drop-shadow-lg text-sm"
+          class=" w-full bg-blue-500 hover:bg-blue-600 active:bg-blue-700 focus:outline-none focus:ring focus:ring-blue-300 rounded-full p-2 m-2 drop-shadow-lg text-sm"
         >
-          Add to card
+          View
         </button>
       </div>
     </div>
